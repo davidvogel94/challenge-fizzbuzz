ca38117097c57a3535a8a885edee5cee1a1efa63
David Vogel <david.vogel@outlook.com.au>
2024-08-25 04:55:09 +0000

    first commit

---
+++ .devcontainer/init-mongo.js
@@ -0,0 +1,13 @@
+db.createUser(
+    {
+        user: "fizzbuzz",
+        pwd: "fizzbuzz",
+        roles: [
+            {
+                role: "readWrite",
+                db: "fizzbuzz"
+            }
+        ]
+    }
+);
+db.createCollection("fizzbuzz"); //MongoDB creates the database when you first store data in that database
---
+++ README.md
@@ -0,0 +1,2 @@
+# challenge-fizzbuzz
+# challenge-fizzbuzz
